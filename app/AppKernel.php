59d5693b161f342c1482544a44595af827cef8ef
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2015-04-13 07:53:23 +0200

    Added contactbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle()
+            new Kristofvc\Bundle\ContactBundle\KristofvcContactBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/bundle/ContactBundle/KristofvcContactBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Kristofvc\Bundle\ContactBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KristofvcContactBundle extends Bundle
+{
+}
