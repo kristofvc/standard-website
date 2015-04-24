85e2da73ff636d6574dc5ebc9e076804c36d9ea2
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2015-04-24 14:00:52 +0200

    Add base javascript

---
+++ src/component/AdminInterface/sources/js/app.js
@@ -0,0 +1,3 @@
+//@koala-prepend "../../components/jquery/dist/jquery.js"
+//@koala-prepend "../../components/bootstrap/dist/js/bootstrap.min.js"
+//@koala-prepend "../../components/jasny-bootstrap/dist/js/jasny-bootstrap.js"
