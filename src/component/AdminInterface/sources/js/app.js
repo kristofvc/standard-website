1bef5d23dc5fcc9a3ed8570726fffb51f0e5833b
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2015-04-25 12:34:54 +0200

    Sliding sidebar

--- src/component/AdminInterface/sources/js/app.js
+++ src/component/AdminInterface/sources/js/app.js
@@ -1,3 +1,15 @@
 //@koala-prepend "../../components/jquery/dist/jquery.js"
 //@koala-prepend "../../components/bootstrap/dist/js/bootstrap.min.js"
 //@koala-prepend "../../components/jasny-bootstrap/dist/js/jasny-bootstrap.js"
+
+$('.navmenu').on('click', '.mainmenu-toggle', function () {
+	if ($('.canvas').hasClass('canvas-closed')) {
+		$('.canvas').removeClass('canvas-closed');
+		$(this).find('.fa').addClass('fa-arrow-left');
+		$(this).find('.fa').removeClass('fa-arrow-right');
+	} else {
+		$('.canvas').addClass('canvas-closed');
+		$(this).find('.fa').addClass('fa-arrow-right');
+		$(this).find('.fa').removeClass('fa-arrow-left');
+	}
+});
