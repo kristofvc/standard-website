ee90e69ba2ca884eda846c10795e680b1148415b
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2015-04-26 14:12:38 +0200

    Disable jasny offcanvas implementation

--- src/component/AdminInterface/sources/js/app.js
+++ src/component/AdminInterface/sources/js/app.js
@@ -2,18 +2,25 @@
 //@koala-prepend "../../components/bootstrap/dist/js/bootstrap.min.js"
 //@koala-prepend "../../components/jasny-bootstrap/dist/js/jasny-bootstrap.js"
 
-$('.navmenu').on('click', '.mainmenu-toggle', function () {
-	if ($('.canvas').hasClass('canvas-closed')) {
-		setTimeout(function () {
-			$('.canvas').removeClass('canvas-closed');
-		}, 210);	
+
+$('.navmenu').on('click', '.mainmenu-toggle', function (e) {
+	e.preventDefault();
+	if ($('.submenu').hasClass('canvas-hide-mainmenu')) {
+		$('.submenu').removeClass('canvas-hide-mainmenu');
 		$(this).find('.fa').addClass('fa-arrow-left');
 		$(this).find('.fa').removeClass('fa-arrow-right');
 	} else {
-		setTimeout(function () {
-			$('.canvas').addClass('canvas-closed');
-		}, 210);	
+		$('.submenu').addClass('canvas-hide-mainmenu');
 		$(this).find('.fa').addClass('fa-arrow-right');
 		$(this).find('.fa').removeClass('fa-arrow-left');
+	}
+});
+
+$('.submenu-toggle').on('click', function (e) {
+	e.preventDefault();
+	if ($('.canvas-inner').hasClass('canvas-hide-submenu')) {
+		$('.canvas-inner').removeClass('canvas-hide-submenu');
+	} else {
+		$('.canvas-inner').addClass('canvas-hide-submenu');
 	}
 });
