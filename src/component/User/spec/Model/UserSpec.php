3664053f3cee48d8f750717cace78a7f0439ab8b
Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
2015-05-15 11:19:46 +0200

    Extend base fos user

--- src/component/User/spec/Model/UserSpec.php
+++ src/component/User/spec/Model/UserSpec.php
@@ -2,13 +2,32 @@
 
 namespace spec\Kristofvc\Component\User\Model;
 
+use Kristofvc\Component\User\Model\User;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * Class UserSpec
+ * @package spec\Kristofvc\Component\User\Model
+ *
+ * @author Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
+ *
+ * @mixin User
+ */
 class UserSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Kristofvc\Component\User\Model\User');
+    }
+
+    function it_should_extend_base_user()
+    {
+        $this->shouldHaveType('FOS\UserBundle\Entity\User');
+    }
+
+    function it_has_no_id_by_default()
+    {
+        $this->getId()->shouldReturn(null);
     }
 }
--- src/component/User/src/Model/User.php
+++ src/component/User/src/Model/User.php
@@ -2,6 +2,15 @@
 
 namespace Kristofvc\Component\User\Model;
 
-class User
+use FOS\UserBundle\Entity\User as BaseUser;
+
+/**
+ * Class User
+ * @package Kristofvc\Component\User\Model
+ *
+ * @author Kristof Van Cauwenbergh <kristof.vancauwenbergh@gmail.com>
+ */
+class User extends BaseUser
 {
+    protected $id;
 }
